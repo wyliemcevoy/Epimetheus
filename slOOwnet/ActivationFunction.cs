d56b9f1fc513917a46658c8f8aa27795032ed315
Wylie McEvoy <wylie.seven@gmail.com>
2016-01-12 23:22:12 -0600

    Extract Activation Function and Learning.
    Extract the learning rate into its own class so it can vary by node and
    other factors.

---
+++ slOOwnet/ActivationFunction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace slOOwnet
+{
+    interface ActivationFunction
+    {
+        double calculate(double input);
+        double calculateGradient(double output);
+    }
+}
