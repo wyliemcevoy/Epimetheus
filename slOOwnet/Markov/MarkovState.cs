6a49754bfd317ed5e488151ec3eb195724b0d086
Wylie McEvoy <wylie.seven@gmail.com>
2016-01-14 21:44:09 -0600

    Add scaffolding for Markov decision problems

---
+++ slOOwnet/Markov/MarkovState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace slOOwnet.Markov
+{
+   
+
+    class MarkovState
+    {
+        public bool isTerminal { get; set; }
+        public double value {get; set; }
+        public double estimatedValue { get; set; }
+        public double nextEstimatedValue { get; set; }
+        public List<StochasticAction> actions { get; private set; }
+        public StochasticAction policy { get; set; }
+        public int index { get; set; }
+
+        public MarkovState ()
+        {
+            this.actions = new List<StochasticAction>();
+        }
+
+    }
+}
